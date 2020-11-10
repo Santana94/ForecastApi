f9300be4509835fb9cf1f0ca94602d5d07e9b0b5
Matheus Menezes Sant'ana <matheusm.santana@outlook.com>
2020-11-10 13:39:54 -0300

    Everything tested so fargit add .

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,7 +1,12 @@
-import pytest
+import os
 
+import pytest
+import tempfile
+
+from app import blueprint
 from app.main import create_app, db
 from pytest_factoryboy import register
+
 
 from app.test.factories import ForecastFactory
 
@@ -22,4 +27,11 @@ def _db(database):
 def app():
     app = create_app('test')
     app.config.from_object('app.main.config.TestingConfig')
+    app.register_blueprint(blueprint)
     return app
+
+
+@pytest.fixture
+def client(app):
+    with app.test_client() as client:
+        yield client
--- app/test/controller/test_forecast_controller.py
+++ app/test/controller/test_forecast_controller.py
@@ -1,8 +1,11 @@
+import json
+
 import requests_mock
 from flask_api import status
 
 from app.main.config import API_ADVISOR_TOKEN, API_ADVISOR_URL
 from app.main.model.forecast import Forecast
+from app.test.api_advisor_return import EXPECTED_JSON
 
 
 def test_city_endpoint_insertion(client):
@@ -11,12 +14,20 @@ def test_city_endpoint_insertion(client):
 
     # THEN
     with requests_mock.Mocker() as mock:
-        mock.get(f'{API_ADVISOR_URL}/{city_id}/days/15?token={API_ADVISOR_TOKEN}', status_code=200, text='')
+        mock.get(f'{API_ADVISOR_URL}{city_id}/days/15?token={API_ADVISOR_TOKEN}',
+                 status_code=status.HTTP_200_OK, text=json.dumps(EXPECTED_JSON))
         response = client.get(f'/cidade?id={city_id}')
 
-    assert response.status_code == 201
-    forecast = Forecast.query.filter_by(id=city_id)
-    assert forecast.city == ''
+    assert response.status_code == status.HTTP_201_CREATED
+    forecast = Forecast.query.filter_by(id=city_id).first()
+    assert forecast.city == EXPECTED_JSON['name']
+    assert forecast.state == EXPECTED_JSON['state']
+    assert forecast.country == EXPECTED_JSON['country']
+    assert forecast.date.strftime('%Y-%m-%d') == EXPECTED_JSON['data'][0]['date']
+    assert forecast.rain_probability == EXPECTED_JSON['data'][0]['rain']['probability']
+    assert forecast.rain_precipitation == EXPECTED_JSON['data'][0]['rain']['precipitation']
+    assert forecast.max_temp == EXPECTED_JSON['data'][0]['temperature']['max']
+    assert forecast.min_temp == EXPECTED_JSON['data'][0]['temperature']['min']
 
 
 def test_city_endpoint_insertion_error(client):
@@ -26,9 +37,10 @@ def test_city_endpoint_insertion_error(client):
     response = client.get('/cidade')
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == {
+    assert response.json == {
         "errors": {
-            "id": "This field is required and should be an integer."
+            "id": 'The city id is required. Missing required parameter in the JSON '
+                  'body or the post body or the query string'
         },
         "message": "Input payload validation failed"
     }
@@ -37,14 +49,16 @@ def test_city_endpoint_insertion_error(client):
 def test_city_endpoint_city_not_found(client):
     # GIVEN
     city_id = 123
+    error_data = {
+        "error": True,
+        "detail": f"Access forbidden, you have no acces for this locale: {city_id}"
+    }
 
     # THEN
     with requests_mock.Mocker() as mock:
-        mock.get(f'{API_ADVISOR_URL}/{city_id}/days/15?token={API_ADVISOR_TOKEN}', status_code=200, text='')
+        mock.get(f'{API_ADVISOR_URL}{city_id}/days/15?token={API_ADVISOR_TOKEN}',
+                 status_code=status.HTTP_400_BAD_REQUEST, text=json.dumps(error_data))
         response = client.get(f'/cidade?id={city_id}')
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data == {
-        "error": True,
-        "detail": "Access forbidden, you have no acces for this locale: 123"
-    }
+    assert response.json == {'error': error_data['detail']}
