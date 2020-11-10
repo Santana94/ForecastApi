9a71d3fdf2029049032b9adff63d625716d5436a
Matheus Menezes Sant'ana <matheusm.santana@outlook.com>
2020-11-10 17:47:54 -0300

    End of implementation

--- app/main/controller/analyze_controller.py
+++ app/main/controller/analyze_controller.py
@@ -27,7 +27,23 @@ class Analyze(Resource):
         if not filtered_query.count():
             return {'message': 'Not data found!'}, status.HTTP_404_NOT_FOUND
 
-        return {}, status.HTTP_200_OK
+        return {
+                   'highest_max_temp': self.get_highest_max_temp_city_data(filtered_query),
+                   'mean_precipitation': self.get_mean_precipitation(filtered_query)
+               }, status.HTTP_200_OK
+
+    @staticmethod
+    def get_highest_max_temp_city_data(queryset):
+        forecast = queryset.order_by(Forecast.max_temp.desc()).first()
+        return {
+            'city': forecast.city,
+            'max_temp': forecast.max_temp
+        }
+
+    @staticmethod
+    def get_mean_precipitation(queryset):
+        precipitations = [i.rain_precipitation for i in queryset]
+        return round(sum(precipitations) / len(precipitations), 2)
 
     @staticmethod
     def filter_queryset(args):
--- app/test/controller/test_analyze_controller.py
+++ app/test/controller/test_analyze_controller.py
@@ -1,3 +1,5 @@
+import random
+
 from flask_api import status
 
 from app.main.service.forecast_service import ForecastService
@@ -60,3 +62,36 @@ def test_analyze_endpoint_ok(client, forecast_data, db_session):
 
     # THEN
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_analyze_endpoint_ok_highest_max_temperature_city_and_mean_precipitation(client, forecast_data, db_session):
+    # GIVEN
+    data = forecast_data
+    data['date'] = convert_datetime('2030-10-10')
+    precipitation_data = []
+    index = 20
+
+    for city, max_temp in [('cidade 1', 10), ('cidade 2', 20), ('cidade 3', 30), ('cidade 4', 40)]:
+        forecast = ForecastService(data)
+        precipitation = random.uniform(0.1, 5)
+        precipitation_data.append(precipitation)
+
+        forecast.data['id'] = index
+        forecast.data['city'] = city
+        forecast.data['max_temp'] = max_temp
+        forecast.data['rain_precipitation'] = precipitation
+        forecast.create()
+        index += 1
+
+    # WHEN
+    response = client.get(f'/analise?data_inicial=2029-10-10&data_final=2031-10-10')
+
+    # THEN
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json == {
+        'highest_max_temp': {
+            'city': 'cidade 4',
+            'max_temp': 40
+        },
+        'mean_precipitation': round(sum(precipitation_data) / len(precipitation_data), 2)
+    }
